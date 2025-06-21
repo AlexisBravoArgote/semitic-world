c5979a9f545e9ff4584dd361913d6747cce2591f
AlexisBravoArgote <abapax@gmail.com>
2025-06-21 22:30:03 +0100

    logo

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <title>Semitic World</title>
+                <meta name="description" content="Translate from any language to any Arabic dialect. Powered by AI." />
+
+                {/* Favicon */}
+                <link rel="icon" href="/logo.png" type="image/png" />
+
+                {/* Open Graph / Social Preview */}
+                <meta property="og:title" content="Semitic World" />
+                <meta property="og:description" content="Translate from any language to any Arabic dialect." />
+                <meta property="og:image" content="https://semiticworld.com/logo.png" />
+                <meta property="og:url" content="https://semiticworld.com" />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
