d580aa595608ab0ee9bfbb58aff2d4c053fa367a
AlexisBravoArgote <abapax@gmail.com>
2025-06-21 22:46:17 +0100

    logo1

--- pages/_document.js
+++ pages/_document.js
@@ -4,18 +4,25 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                {/* Title and Description */}
                 <title>Semitic World</title>
                 <meta name="description" content="Translate from any language to any Arabic dialect. Powered by AI." />
 
                 {/* Favicon */}
                 <link rel="icon" href="/logo.png" type="image/png" />
 
-                {/* Open Graph / Social Preview */}
+                {/* Open Graph / Social Sharing */}
                 <meta property="og:title" content="Semitic World" />
                 <meta property="og:description" content="Translate from any language to any Arabic dialect." />
                 <meta property="og:image" content="https://semiticworld.com/logo.png" />
                 <meta property="og:url" content="https://semiticworld.com" />
+                <meta property="og:type" content="website" />
+
+                {/* Twitter Card */}
                 <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Semitic World" />
+                <meta name="twitter:description" content="Translate from any language to any Arabic dialect." />
+                <meta name="twitter:image" content="https://semiticworld.com/logo.png" />
             </Head>
             <body>
                 <Main />
@@ -24,3 +31,4 @@ export default function Document() {
         </Html>
     );
 }
+
